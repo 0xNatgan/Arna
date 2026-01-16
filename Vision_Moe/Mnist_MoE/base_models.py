67a690e84a7a0a567070f31b84e2cf9493a56b88
Nathan <Fitger@>
2026-01-16 21:42:26 +0100

    changes to base_moe

--- Vision_Moe/Mnist_MoE/base_models.py
+++ Vision_Moe/Mnist_MoE/base_models.py
@@ -22,19 +22,25 @@ class MNIST_CNN1(nn.Module):
 class MNIST_CNN2(nn.Module):
     def __init__(self):
         super(MNIST_CNN2, self).__init__()
-        # layers : conv1 (k5) -> MaxPool -> conv2 (k5) -> linear 128-256 -> linear2 256-10 -> ReLU(output)
+        # Enhanced layers : conv1 (k5) -> BN -> MaxPool -> conv2 (k5) -> BN -> linear -> Dropout -> linear2
         self.conv1 = nn.Conv2d(1, 64, kernel_size=5, padding=2)
+        self.bn1 = nn.BatchNorm2d(64)
         self.pool = nn.MaxPool2d(2, 2)
+
         self.conv2 = nn.Conv2d(64, 128, kernel_size=5, padding=2)
+        self.bn2 = nn.BatchNorm2d(128)
+
         self.fc1 = nn.Linear(128 * 7 * 7, 256)
+        self.dropout = nn.Dropout(0.5)
         self.fc2 = nn.Linear(256, 10) # 10 output classes
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))
         x = x.view(-1, 128 * 7 * 7) # Flatten
         x = self.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
 
@@ -56,36 +62,49 @@ class MNIST_CNN3(nn.Module):
 class MNIST_MediumCNN(nn.Module):
     def __init__(self):
         super(MNIST_MediumCNN, self).__init__()
-        # Standard robust architecture: 3 Conv layers, dense head
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
+        # Enhanced architecture with double convolution blocks
+        self.block1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
         
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
+        self.block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
         
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
-        
-        self.pool = nn.MaxPool2d(2, 2)
-        self.relu = nn.ReLU()
+        self.block3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
         
         # After 3 pools (28 -> 14 -> 7 -> 3)
-        self.fc1 = nn.Linear(128 * 3 * 3, 256)
-        self.dropout = nn.Dropout(0.4)
-        self.fc2 = nn.Linear(256, 10)
+        self.fc_layers = nn.Sequential(
+            nn.Linear(128 * 3 * 3, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, 10)
+        )
 
     def forward(self, x):
-        # 1. Conv-BN-ReLU-Pool (28 -> 14)
-        x = self.pool(self.relu(self.bn1(self.conv1(x))))
-        
-        # 2. Conv-BN-ReLU-Pool (14 -> 7)
-        x = self.pool(self.relu(self.bn2(self.conv2(x))))
-        
-        # 3. Conv-BN-ReLU-Pool (7 -> 3)
-        x = self.pool(self.relu(self.bn3(self.conv3(x))))
-        
-        x = x.view(-1, 128 * 3 * 3) # Flatten
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = x.view(-1, 128 * 3 * 3)
+        x = self.fc_layers(x)
         return x
